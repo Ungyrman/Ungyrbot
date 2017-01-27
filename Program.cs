dce47553ee9611098fb03a5463945fcafa200765
Ungyrman <pleasantloudmouth@gmail.com>
2017-01-27 17:52:28 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ungyrbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }
+    }
+}
